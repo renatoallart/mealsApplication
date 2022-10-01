fd59eb5feaa9a5a279ee2d708fc92772b122f603
Renato Santos <renato.senstec@gmail.com>
2022-10-01 14:19:44 -0300

    fetch data and meals component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,13 +1,39 @@
 import { useGlobalContext } from '../AppProvider'
-
+import { BsHandThumbsUp } from 'react-icons/bs'
 
 export function Meals() {
 
-  const mealsContext = useGlobalContext()
+  const { meals, isLoading } = useGlobalContext()
 
+
+  if (isLoading) {
+    return (
+      <section className="section">
+        <h4> Loading...</h4>
+      </section>
+    )
+  }
+
+  if(meals.length < 1){
+    return (
+      <section className="section">
+        <h4> No meals match ur search term. Please try again.</h4>
+      </section>
+    )
+  }
   return (
-    <div>
-      {mealsContext}
-    </div>
+    <section className='section-center'>
+      {meals.map(meal => {
+        const { idMeal, strMeal: title, strMealThumb: image } = meal
+        return <article key={idMeal} className='single-meal'>
+          <img src={image} className='img' alt="" />
+          <footer>
+            <h5>{title}</h5>
+            <button className='like-btn'> <BsHandThumbsUp /></button>
+          </footer>
+
+        </article>
+      })}
+    </section>
   )
 }

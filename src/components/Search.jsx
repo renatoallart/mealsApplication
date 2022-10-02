dd653f1aa9c8016ada94f653289cf8565c9b188a
Renato Santos <renato.senstec@gmail.com>
2022-10-02 17:27:42 -0300

    Search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
+import { useGlobalContext } from '../AppProvider'
 
-export default function Search() {
+export function Search() {
+
+  const [nameInput,setNameInput] = useState('')
+  const {meals} = useGlobalContext()
+
+  function handleChange(event){
+    const {value} = event.target
+    setNameInput(value)
+  }
+
+  function searchMealByName(name){
+    if(name == null) return 
+    return (
+      <div>
+        {meals.filter(meal => meal.strMeal === name).map(meal => <p>{meal.strMeal}</p> )}
+      </div>
+    )
+  }
+
+
   return (
-    <div>Search</div>
+    <header className="search-container">
+      <div>
+        {nameInput}
+      </div>
+      <input 
+        onChange={event => handleChange(event)}
+        name='nameInput'
+        value={nameInput}
+        className='form-input'
+        type="text" />
+      <button type='submit' onClick={()=> searchMealByName(nameInput)} className='btn'>Submit</button>
+      <button type='btn' className='btn btn-hipster'>Surprise me!</button>
+    </header>
   )
 }
